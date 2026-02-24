b0b45c41ec9601b66af8ec255665655e9463b925
fegeirfan <fegeirfan@gmail.com>
2026-02-24 14:25:19 +0700

    kk

---
+++ index.js
@@ -0,0 +1,27 @@
+
+import express from "express";
+const app = express();
+
+app.use((req, res, next) => {
+  const eror = true;
+
+  if (!eror) {
+    const err = new Error("Not Authorized");
+    err.status = 401; 
+    return next(err);
+  }
+
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello irfan!!!');
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send('Error');
+});
+
+app.listen(3000, () => {
+  console.log('Server jalan di port 3000');
+});
