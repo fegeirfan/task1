ac1a6bfa1750f1dc3096d82c812020f10c94a992
fegeirfan <fegeirfan@gmail.com>
2026-02-24 15:55:02 +0700

    kk

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-
 import express from "express";
 const app = express();
 
-app.use((req, res, next) => {
-  const eror = true;
-
-  if (!eror) {
-    const err = new Error("Not Authorized");
-    err.status = 401; 
-    return next(err);
-  }
-
-  next();
+app.get("/", (req, res) => {
+  res.send("Hello irfan!!!");
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello irfan!!!');
+app.get("/gogo", (req, res) => {
+  res.send("Hello gogo!!!");
 });
-app.get('/gogo', (req, res) => {
-  res.send('Hello gogo!!!');
+
+// 🔥 rute yang sengaja error
+app.get("/error", (req, res, next) => {
+  const err = new Error("Not Authorized");
+  err.status = 401;
+  next(err);
 });
+
+// error handler (WAJIB di paling bawah)
 app.use((err, req, res, next) => {
-  res.status(500).send('Error');
+  res
+    .status(err.status || 500)
+    .send(err.message || "Server Error");
 });
 
 app.listen(3000, () => {
-  console.log('Server jalan di port 3000');
+  console.log("Server jalan di port 3000");
 });
