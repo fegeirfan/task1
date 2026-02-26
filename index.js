826685b0ef3dbf4da34417aca626103022331348
fegeirfan <fegeirfan@gmail.com>
2026-02-26 16:44:23 +0700

    ddj

--- index.js
+++ index.js
@@ -8,10 +8,19 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"], credentials: true }));
 
-// koneksi MongoDB
-mongoose.connect('mongodb+srv://fegeirfan_db_user:Cikarang123@cluster0.fnk9fp9.mongodb.net/?appName=Cluster0')
-  .then(() => { console.log('Database connected!'); })
-  .catch((err) => { console.error('Database connection error:', err); });
+// koneksi MongoDB dengan await
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://fegeirfan_db_user:Cikarang123@cluster0.fnk9fp9.mongodb.net/?appName=Cluster0', {
+      serverSelectionTimeoutMS: 30000,
+      socketTimeoutMS: 45000,
+    });
+    console.log('Database connected!');
+  } catch (err) {
+    console.error('Database connection error:', err);
+    process.exit(1);
+  }
+};
 
 // rute
 app.use("/posts", postsRouter);
@@ -36,6 +45,12 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).send(err.message || "Server Error");
 });
 
-app.listen(3000, () => {
-  console.log("Server jalan di port localhost:3000");
-});
+// mulai server setelah koneksi MongoDB berhasil
+const startServer = async () => {
+  await connectDB();
+  app.listen(3000, () => {
+    console.log("Server jalan di port localhost:3000");
+  });
+};
+
+startServer();
