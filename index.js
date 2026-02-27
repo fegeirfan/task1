bdd7f874f46170f1b51082f23851e482f81cd3ae
fegeirfan <fegeirfan@gmail.com>
2026-02-27 13:43:41 +0700

    dd

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import postsRouter from "./routes/posts.js";
 import cors from "cors";
 
 const app = express();
+app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"], allowedHeaders: ["Content-Type"] }));
 app.use(express.json());
-app.use(cors({ origin: "*" }));
 
 // koneksi MongoDB dengan await
 const connectDB = async () => {
